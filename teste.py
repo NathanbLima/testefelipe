9f91253e56351426c684a5e2753f045dc067ba43
NathanbLima <134015217+NathanbLima@users.noreply.github.com>
2023-05-30 01:18:43 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,65 +1,48 @@
-import streamlit as st
 import pandas as pd
 
-# Carrega os dados de produção a partir do arquivo Excel ou cria um novo DataFrame vazio
-try:
-    producao_df = pd.read_excel('dados_producao.xlsx')
-except FileNotFoundError:
-    producao_df = pd.DataFrame(columns=['Produto', 'Quantidade', 'Defeitos'])
+# Criar um DataFrame vazio para armazenar os dados
+dados_producao = pd.DataFrame(columns=['Dia', 'Produto', 'Quantidade', 'Defeitos'])
 
-# Função para salvar os dados de produção no arquivo Excel
-def salvar_dados_producao():
-    producao_df.to_excel('dados_producao.xlsx', index=False)
+while True:
+    print("========== MENU ==========")
+    print("1 - Registrar produção")
+    print("2 - Registrar defeitos")
+    print("3 - Mostrar estatísticas")
+    print("0 - Sair")
+    opcao = input("Selecione uma opção: ")
 
-# Função para registrar a produção
-def registrar_producao():
-    global producao_df
-    produto = st.text_input("Digite o nome do produto:")
-    quantidade = st.number_input("Digite a quantidade produzida:", min_value=0, step=1, value=0)
+    if opcao == '1':
+        dia = input("Digite o dia da produção: ")
+        produto = input("Digite o produto: ")
+        quantidade = int(input("Digite a quantidade produzida: "))
 
-    if produto in producao_df['Produto'].values:
-        producao_df.loc[producao_df['Produto'] == produto, 'Quantidade'] += quantidade
-    else:
-        novo_produto = pd.DataFrame({'Produto': [produto], 'Quantidade': [quantidade], 'Defeitos': [0]})
-        producao_df = producao_df.append(novo_produto, ignore_index=True)
+        dados_producao = dados_producao.append({
+            'Dia': dia,
+            'Produto': produto,
+            'Quantidade': quantidade,
+            'Defeitos': 0
+        }, ignore_index=True)
 
-    # Verifica se a coluna "Defeitos" existe no DataFrame
-    if 'Defeitos' not in producao_df.columns:
-        producao_df['Defeitos'] = 0  # Adiciona a coluna "Defeitos" com valores iniciais de zero
+    elif opcao == '2':
+        dia = input("Digite o dia em que ocorreu o defeito: ")
+        produto = input("Digite o produto com defeito: ")
+        defeitos = int(input("Digite a quantidade de defeitos: "))
 
-    # Salva os dados atualizados no arquivo Excel
-    salvar_dados_producao()
+        # Atualizar o número de defeitos para o produto e dia correspondentes
+        dados_producao.loc[(dados_producao['Dia'] == dia) & (dados_producao['Produto'] == produto), 'Defeitos'] += defeitos
 
-# Função para registrar os defeitos
-def registrar_defeitos():
-    global producao_df
-    produto = st.text_input("Digite o nome do produto:")
-    defeitos = st.number_input("Digite a quantidade de defeitos:", min_value=0, step=1, value=0)
+    elif opcao == '3':
+        # Mostrar as estatísticas
+        print("======== Estatísticas ========")
+        print("Peça com maior índice de reprovação:")
+        print(dados_producao.groupby('Produto')['Defeitos'].sum().idxmax())
+        print("Peça com maior quantidade de produção:")
+        print(dados_producao.groupby('Produto')['Quantidade'].sum().idxmax())
+        print("Média de erros semanais:")
+        print(dados_producao['Defeitos'].mean())
 
-    # Atualiza a quantidade de defeitos para o produto selecionado
-    producao_df.loc[producao_df['Produto'] == produto, 'Defeitos'] += defeitos
+    elif opcao == '0':
+        break
 
-    # Salva os dados atualizados no arquivo Excel
-    salvar_dados_producao()
-
-# Função para exibir os dados de produção e informações adicionais
-def exibir_dados_producao():
-    st.subheader("Dados de Produção")
-    st.write(producao_df)
-
-    # Converte a coluna "Defeitos" para o tipo numérico
-    producao_df['Defeitos'] = pd.to_numeric(producao_df['Defeitos'])
-
-    produto_reprovacao_semanal = producao_df.loc[producao_df['Defeitos'].idxmax(), 'Produto']
-    produto_maior_producao = producao_df.loc[producao_df['Quantidade'].idxmax(), 'Produto']
-    media_erros_semanais = producao_df['Defeitos'].mean()
-
-    st.subheader("Informações para Apoio a Decisões")
-    st.write("Peça com maior índice de reprovação semanal:", produto_reprovacao_semanal)
-    st.write("Peça com maior quantidade de produção:", produto_maior_producao)
-    st.write("Média de erros semanais:", media_erros_semanais)
-
-# Configurações iniciais
-st.set_page_config(page_title="Análise de Produção", layout="wide")
-
-#
+# Salvar os dados em uma planilha Excel
+dados_producao.to_excel('dados_producao.xlsx', index=False)
