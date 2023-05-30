fdb3f7a1cb1c54db18796215bdda24176fb88572
NathanbLima <134015217+NathanbLima@users.noreply.github.com>
2023-05-30 01:20:03 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,20 +1,17 @@
+import streamlit as st
 import pandas as pd
 
 # Criar um DataFrame vazio para armazenar os dados
 dados_producao = pd.DataFrame(columns=['Dia', 'Produto', 'Quantidade', 'Defeitos'])
 
 while True:
-    print("========== MENU ==========")
-    print("1 - Registrar produção")
-    print("2 - Registrar defeitos")
-    print("3 - Mostrar estatísticas")
-    print("0 - Sair")
-    opcao = input("Selecione uma opção: ")
+    st.sidebar.title("Menu")
+    opcao = st.sidebar.radio("Selecione uma opção:", ("Registrar produção", "Registrar defeitos", "Mostrar estatísticas"))
 
-    if opcao == '1':
-        dia = input("Digite o dia da produção: ")
-        produto = input("Digite o produto: ")
-        quantidade = int(input("Digite a quantidade produzida: "))
+    if opcao == "Registrar produção":
+        dia = st.text_input("Digite o dia da produção:")
+        produto = st.text_input("Digite o produto:")
+        quantidade = st.number_input("Digite a quantidade produzida:", min_value=0, value=0, step=1)
 
         dados_producao = dados_producao.append({
             'Dia': dia,
@@ -23,25 +20,25 @@ while True:
             'Defeitos': 0
         }, ignore_index=True)
 
-    elif opcao == '2':
-        dia = input("Digite o dia em que ocorreu o defeito: ")
-        produto = input("Digite o produto com defeito: ")
-        defeitos = int(input("Digite a quantidade de defeitos: "))
+    elif opcao == "Registrar defeitos":
+        dia = st.text_input("Digite o dia em que ocorreu o defeito:")
+        produto = st.text_input("Digite o produto com defeito:")
+        defeitos = st.number_input("Digite a quantidade de defeitos:", min_value=0, value=0, step=1)
 
         # Atualizar o número de defeitos para o produto e dia correspondentes
         dados_producao.loc[(dados_producao['Dia'] == dia) & (dados_producao['Produto'] == produto), 'Defeitos'] += defeitos
 
-    elif opcao == '3':
+    elif opcao == "Mostrar estatísticas":
         # Mostrar as estatísticas
-        print("======== Estatísticas ========")
-        print("Peça com maior índice de reprovação:")
-        print(dados_producao.groupby('Produto')['Defeitos'].sum().idxmax())
-        print("Peça com maior quantidade de produção:")
-        print(dados_producao.groupby('Produto')['Quantidade'].sum().idxmax())
-        print("Média de erros semanais:")
-        print(dados_producao['Defeitos'].mean())
+        st.subheader("Estatísticas")
+        st.write("Peça com maior índice de reprovação:")
+        st.write(dados_producao.groupby('Produto')['Defeitos'].sum().idxmax())
+        st.write("Peça com maior quantidade de produção:")
+        st.write(dados_producao.groupby('Produto')['Quantidade'].sum().idxmax())
+        st.write("Média de erros semanais:")
+        st.write(dados_producao['Defeitos'].mean())
 
-    elif opcao == '0':
+    if not st.sidebar.button("Continuar"):
         break
 
 # Salvar os dados em uma planilha Excel
