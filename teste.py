c5bbe3be94e549b8173734f4c45d4b9c2c34e17f
NathanbLima <134015217+NathanbLima@users.noreply.github.com>
2023-05-30 01:58:10 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,76 +1,47 @@
 import streamlit as st
 import pandas as pd
 
-# Função para carregar os dados de produção do cache ou criar uma lista vazia
-@st.cache
-def carregar_dados_producao():
-    return []
+# Função para ler o arquivo xlsx ou criar um novo DataFrame vazio
+def ler_dados_producao():
+    try:
+        df = pd.read_excel('dados_producao.xlsx')
+    except FileNotFoundError:
+        df = pd.DataFrame(columns=['Produto', 'Quantidade', 'Defeitos'])
+    return df
+
+# Função para salvar os dados no arquivo xlsx
+def salvar_dados_producao(df):
+    df.to_excel('dados_producao.xlsx', index=False)
 
 # Carrega os dados de produção
-lista_dados_producao = carregar_dados_producao()
+df_dados_producao = ler_dados_producao()
 
-# Menu lateral com 3 segmentações
+# Menu lateral
 opcao = st.sidebar.radio("Selecione uma opção:", ("Registrar produção", "Registrar defeitos", "Mostrar estatísticas"))
 
-
-# Botão para zerar os dados
-if st.button("Zerar todos os dados"):
-    lista_dados_producao = []
-    lista_dados_defeitos = []
-    st.cache.clear()
-    st.success("Todos os dados foram zerados com sucesso!")
-
-
-
-
-# Segmentação "Registrar produção"
 if opcao == "Registrar produção":
-    st.subheader("Registrar produção")
-    produto = st.text_input("Digite o nome do produto:")
-    quantidade = st.number_input("Digite a quantidade de peças produzidas:", min_value=0, step=1)
+    st.header("Registrar produção")
+    produto = st.text_input("Nome do produto (chave única)")
+    quantidade = st.number_input("Quantidade de peças produzidas", min_value=0)
+    
     if st.button("Salvar"):
-        produto_encontrado = False
-        for item in lista_dados_producao:
-            if item['Produto'] == produto:
-                item['Quantidade'] += quantidade
-                produto_encontrado = True
-                break
-        if not produto_encontrado:
-            lista_dados_producao.append({
-                'Produto': produto,
-                'Quantidade': quantidade,
-                'Defeitos': 0
-            })
-        st.success("Dados de produção registrados com sucesso!")
-
-# Segmentação "Registrar defeitos"
-elif opcao == "Registrar defeitos":
-    st.subheader("Registrar defeitos")
-    produto = st.text_input("Digite o nome do produto:")
-    defeitos = st.number_input("Digite a quantidade de peças defeituosas:", min_value=0, step=1)
-    if st.button("Salvar"):
-        produto_encontrado = False
-        for item in lista_dados_producao:
-            if item['Produto'] == produto:
-                item['Defeitos'] += defeitos
-                produto_encontrado = True
-                break
-        if produto_encontrado:
-            st.success("Dados de defeitos registrados com sucesso!")
+        # Verifica se o nome do produto já existe no DataFrame
+        if produto in df_dados_producao['Produto'].values:
+            # Atualiza a linha correspondente
+            df_dados_producao.loc[df_dados_producao['Produto'] == produto, 'Quantidade'] = quantidade
         else:
-            st.error("Produto não encontrado!")
-                
-            
-# Segmentação "Mostrar estatísticas"
+            # Adiciona uma nova linha
+            df_dados_producao = df_dados_producao.append({'Produto': produto, 'Quantidade': quantidade, 'Defeitos': 0}, ignore_index=True)
+        
+        # Salva os dados no arquivo xlsx
+        salvar_dados_producao(df_dados_producao)
+
+        st.success("Informações salvas com sucesso!")
+
+elif opcao == "Registrar defeitos":
+    st.header("Registrar defeitos")
+    # Código para registrar defeitos
+
 elif opcao == "Mostrar estatísticas":
-    st.subheader("Estatísticas")
-    st.write("Tabela de produção e defeitos por produto:")
-    df_dados_producao = pd.DataFrame(lista_dados_producao)
-    st.table(df_dados_producao)
-
-# Salva os dados de produção atualizados no cache
-st.cache(func=carregar_dados_producao)(lista_dados_producao)
-
-# Exibir DataFrame atualizado
-st.write("Dados de produção atualizados:")
-st.write(pd.DataFrame(lista_dados_producao))
+    st.header("Mostrar estatísticas")
+    # Código para mostrar estatísticas
