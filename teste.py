ef8ba5256c538c387144165ba362227c5e4c0748
NathanbLima <134015217+NathanbLima@users.noreply.github.com>
2023-05-30 01:23:18 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-# Criar um DataFrame vazio para armazenar os dados
-dados_producao = pd.DataFrame(columns=['Produto', 'Quantidade', 'Defeitos'])
+# Criar uma lista vazia para armazenar os dados
+dados_producao = []
 
 while True:
     st.sidebar.title("Menu")
@@ -12,31 +12,37 @@ while True:
         produto = st.text_input("Digite o produto:")
         quantidade = st.number_input("Digite a quantidade produzida:", min_value=0, value=0, step=1)
 
-        dados_producao = dados_producao.append({
+        dados_producao.append({
             'Produto': produto,
             'Quantidade': quantidade,
             'Defeitos': 0
-        }, ignore_index=True)
+        })
 
     elif opcao == "Registrar defeitos":
         produto = st.text_input("Digite o produto com defeito:")
         defeitos = st.number_input("Digite a quantidade de defeitos:", min_value=0, value=0, step=1)
 
         # Atualizar o número de defeitos para o produto correspondente
-        dados_producao.loc[dados_producao['Produto'] == produto, 'Defeitos'] += defeitos
+        for item in dados_producao:
+            if item['Produto'] == produto:
+                item['Defeitos'] += defeitos
 
     elif opcao == "Mostrar estatísticas":
+        # Converter a lista de dados para DataFrame
+        df_dados_producao = pd.DataFrame(dados_producao)
+
         # Mostrar as estatísticas
         st.subheader("Estatísticas")
         st.write("Peça com maior índice de reprovação:")
-        st.write(dados_producao.groupby('Produto')['Defeitos'].sum().idxmax())
+        st.write(df_dados_producao.groupby('Produto')['Defeitos'].sum().idxmax())
         st.write("Peça com maior quantidade de produção:")
-        st.write(dados_producao.groupby('Produto')['Quantidade'].sum().idxmax())
+        st.write(df_dados_producao.groupby('Produto')['Quantidade'].sum().idxmax())
         st.write("Média de erros semanais:")
-        st.write(dados_producao['Defeitos'].mean())
+        st.write(df_dados_producao['Defeitos'].mean())
 
     if not st.sidebar.button("Continuar"):
         break
 
 # Salvar os dados em uma planilha Excel
-dados_producao.to_excel('dados_producao.xlsx', index=False)
+df_dados_producao = pd.DataFrame(dados_producao)
+df_dados_producao.to_excel('dados_producao.xlsx', index=False)
