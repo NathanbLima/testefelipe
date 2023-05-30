a4f3f9476111c5273adba3e97aee6d9aad7b6cd2
NathanbLima <134015217+NathanbLima@users.noreply.github.com>
2023-05-30 01:36:14 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,44 +1,43 @@
 import streamlit as st
 import pandas as pd
 
-# Criar um DataFrame vazio para armazenar os dados
+# Criação de um DataFrame vazio
 df_dados_producao = pd.DataFrame(columns=['Produto', 'Quantidade', 'Defeitos'])
 
-while True:
-    st.sidebar.title("Menu")
-    opcao = st.sidebar.radio("Selecione uma opção:", ("Registrar produção", "Registrar defeitos", "Mostrar estatísticas"), key="menu_opcao")
+# Menu lateral com 3 segmentações
+opcao = st.sidebar.radio("Selecione uma opção:", ("Registrar produção", "Registrar defeitos", "Mostrar estatísticas"))
 
-    if opcao == "Registrar produção":
-        produto = st.text_input("Digite o produto:", key="produto")
-        quantidade = st.number_input("Digite a quantidade produzida:", min_value=0, value=0, step=1, key="quantidade")
-
+# Segmentação "Registrar produção"
+if opcao == "Registrar produção":
+    st.subheader("Registrar produção")
+    produto = st.text_input("Digite o nome do produto:")
+    quantidade = st.number_input("Digite a quantidade de peças produzidas:", min_value=0, step=1)
+    if st.button("Salvar"):
         df_dados_producao = df_dados_producao.append({
             'Produto': produto,
             'Quantidade': quantidade,
             'Defeitos': 0
         }, ignore_index=True)
+        st.success("Dados de produção registrados com sucesso!")
 
-    elif opcao == "Registrar defeitos":
-        produto = st.text_input("Digite o produto com defeito:", key="produto_defeito")
-        defeitos = st.number_input("Digite a quantidade de defeitos:", min_value=0, value=0, step=1, key="quantidade_defeitos")
-
-        # Atualizar o número de defeitos para o produto correspondente
-        df_dados_producao.loc[df_dados_producao['Produto'] == produto, 'Defeitos'] += defeitos
-
-    elif opcao == "Mostrar estatísticas":
-        if df_dados_producao.empty:
-            st.warning("Nenhum dado de produção registrado.")
+# Segmentação "Registrar defeitos"
+elif opcao == "Registrar defeitos":
+    st.subheader("Registrar defeitos")
+    produto = st.text_input("Digite o nome do produto:")
+    defeitos = st.number_input("Digite a quantidade de peças defeituosas:", min_value=0, step=1)
+    if st.button("Salvar"):
+        if produto in df_dados_producao['Produto'].values:
+            df_dados_producao.loc[df_dados_producao['Produto'] == produto, 'Defeitos'] += defeitos
+            st.success("Dados de defeitos registrados com sucesso!")
         else:
-            st.subheader("Estatísticas")
-            st.write("Peça com maior índice de reprovação:")
-            st.write(df_dados_producao.groupby('Produto')['Defeitos'].sum().idxmax())
-            st.write("Peça com maior quantidade de produção:")
-            st.write(df_dados_producao.groupby('Produto')['Quantidade'].sum().idxmax())
-            st.write("Média de erros semanais:")
-            st.write(df_dados_producao['Defeitos'].mean())
+            st.error("Produto não encontrado!")
 
-    if not st.sidebar.button("Continuar", key="continuar"):
-        break
+# Segmentação "Mostrar estatísticas"
+elif opcao == "Mostrar estatísticas":
+    st.subheader("Estatísticas")
+    st.write("Tabela de produção e defeitos por produto:")
+    st.table(df_dados_producao)
 
-# Salvar os dados em uma planilha Excel
-df_dados_producao.to_excel('dados_producao.xlsx', index=False)
+# Exibir DataFrame atualizado
+st.write("Dados de produção atualizados:")
+st.write(df_dados_producao)
